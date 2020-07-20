ba4c76552a89694d4aa88fe918ce989d2563c98f
Kimberly Boyd <kimberlyboyd@Kimberlys-MacBook-Pro.local>
2020-07-19 20:07:30 -0400

    bubblepage done i think

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -8,6 +9,23 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  const componentDidMount = () => {
+    console.log('Component mounted')
+    fetchColors()
+  };
+
+  const fetchColors = () => {
+    axiosWithAuth()
+      .get('/colors')
+      .then(res => {
+        console.log({ res });
+        setColorList(res.data);
+      })
+      .catch(err => console.log({ err }));
+  }
+
+
 
   return (
     <>
